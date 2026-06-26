09835309cec83ea7ae9c635ffa571950b979ed88
Jagdish Kamlekar <90018155+Jagdish-kamlekar@users.noreply.github.com>
2026-06-26 12:19:41 +0530

    create files

---
+++ ec-auth/ec-auth/src/test/java/easycoder/com/ec_auth/EcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package easycoder.com.ec_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
